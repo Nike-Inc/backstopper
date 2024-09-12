7d18bd952f43f761098ecd70e86652eac4c8688f
Nic Munroe <nic.munroe@gmail.com>
2024-09-11 18:24:02 -0700

    Add testonly modules for springboot 3.2.x for webmvc and webflux

---
+++ testonly/testonly-springboot3_2-webmvc/src/test/java/serverconfig/directimport/Springboot3_2WebMvcDirectImportConfig.java
@@ -0,0 +1,51 @@
+package serverconfig.directimport;
+
+import com.nike.backstopper.apierror.projectspecificinfo.ProjectApiErrors;
+import com.nike.backstopper.handler.springboot.config.BackstopperSpringboot3WebMvcConfig;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.Ordered;
+
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import testonly.componenttest.spring.reusable.controller.SampleController;
+import testonly.componenttest.spring.reusable.error.SampleProjectApiErrorsImpl;
+import testonly.componenttest.spring.reusable.testutil.ExplodingServletFilter;
+
+/**
+ * Springboot config that uses {@link Import} to integrate Backstopper via direct import of
+ * {@link BackstopperSpringboot3WebMvcConfig}.
+ *
+ * @author Nic Munroe
+ */
+@SpringBootApplication
+@Import({
+    // Import core Backstopper+Springboot1 support.
+    BackstopperSpringboot3WebMvcConfig.class,
+    // Import the controller.
+    SampleController.class
+})
+@SuppressWarnings("unused")
+public class Springboot3_2WebMvcDirectImportConfig {
+
+    @Bean
+    public ProjectApiErrors getProjectApiErrors() {
+        return new SampleProjectApiErrorsImpl();
+    }
+
+    @Bean
+    public Validator getJsr303Validator() {
+        //noinspection resource
+        return Validation.buildDefaultValidatorFactory().getValidator();
+    }
+
+    @Bean
+    public FilterRegistrationBean<?> explodingServletFilter() {
+        FilterRegistrationBean<?> frb = new FilterRegistrationBean<>(new ExplodingServletFilter());
+        frb.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return frb;
+    }
+}
